61d1548ac66a9650cf9e559c8ddd0cbbd58b8e80
Mahbubur Rahman <mahbub.apece@gmail.com>
2014-11-05 21:23:41 +0600

    MaxNumberDetection

---
+++ MaxNumberApp/MaxNumberApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxNumberApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] i = new int[7] {12, 15, 46, 48, 79, 12, 45};
+            int largest = i[0];
+            //for (int j = 0; j < 6; j++)
+            //{
+            //    if (i[j+1]>=largest)
+            //    {
+            //        largest = i[j + 1];
+            //    }
+            //}
+
+            foreach (int i1 in i)
+            {
+                if (largest<=i1)
+                {
+                    largest = i1;
+                }
+            }
+
+            Console.WriteLine(largest);
+            Console.ReadKey();
+        }
+    }
+}
