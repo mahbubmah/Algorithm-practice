59c14aea1c2add9507d27c0ef8261509b403bc3d
Mahbubur Rahman <mahbub.apece@gmail.com>
2014-11-05 21:41:34 +0600

    LinierSearchAppAdded

---
+++ LinearSearchApp/LinearSearchApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearSearchApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums=new int[5]{15,56,45,48,78};
+
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            bool found = false;
+
+            foreach (int num in nums)
+            {
+                
+                if (num==x)
+                {
+                    Console.WriteLine("got it");
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+            {
+                Console.WriteLine("No match found");
+            }
+            Console.ReadKey();
+        }
+    }
+}
