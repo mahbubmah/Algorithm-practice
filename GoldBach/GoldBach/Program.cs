918b72c5ecbb3552a0adafba1cf7b648740f487f
Mahbubur Rahman <mahbub.apece@gmail.com>
2014-11-08 05:31:17 +0600

    Two program added

---
+++ GoldBach/GoldBach/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoldBach
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int h = 0, l = 0, r = 0, x = 0, count = 0;
+
+            l = Convert.ToInt32(Console.ReadLine());
+
+            h = Convert.ToInt32(Console.ReadLine());
+            int gc = 0;
+
+            for (int i = l; i <= h; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    int k = 0;
+                    for (int j = 0; j <= i; j++)
+                    {
+                        k = i - j;
+                        if (IsPrime(k) && IsPrime(j))
+                        {
+                            if (i==j+k)
+                            {
+                                count++;
+                            }
+                        }
+                    }
+
+                    gc = gc+(count/2);
+                    count = 0;
+                }
+            }
+            string a = gc.ToString();
+            Console.WriteLine(gc);
+            Console.ReadKey();
+        }
+
+        private static bool IsPrime(int i)
+        {
+            if (i == 0)
+            {
+                return false;
+            }
+            if (i == 1)
+            {
+                return false;
+            }
+
+            for (int j = 2; j < i / 2; j++)
+            {
+                
+                if (i % j == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ King’sWishApp/King’sWishApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace King_sWishApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong c = 0,r=0,x=0;
+            
+            c=Convert.ToUInt32(Console.ReadLine());
+            
+
+            for (ulong i = c-1; i >=1; i--)
+            {
+                for (ulong j = 2; j <c-1; j++)
+                {
+                    r = i*j;
+                    if (c==r)
+                    {
+                        if (i>j)
+                        {
+                            string b=i+","+j;
+                            Console.WriteLine(b);
+                            goto n;
+                        }
+                    }
+                }
+                if (i==1)
+                {
+                    string a = "Impossible";
+                    Console.WriteLine(a);
+
+                }
+            }
+            n:
+            Console.ReadKey();
+        }
+    }
+}
